6f283f175f2c78fb5a7e7b9b6c06acd5e1711ace
jafaar-eng <joker2000710@gmail.com>
2026-08-13 22:42:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY";
+window.SUPABASE_URL = "https://aofnrqkbgshrlizbiqyt.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_nzZndx8C8gLp7MMaDwelDA_6O3jEipd";
 window.PUBLIC_BASE_URL = "";
